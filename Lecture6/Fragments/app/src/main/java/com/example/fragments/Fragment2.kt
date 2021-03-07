b5d5047c6967467762c8bc58cef1de8dc36cd428
Victor Kyhe Clemmensen <victorclemmensen@Victors-MacBook-Air.local>
2021-03-07 19:37:08 +0100

    using lifecycles

--- Lecture6/Fragments/app/src/main/java/com/example/fragments/Fragment2.kt
+++ Lecture6/Fragments/app/src/main/java/com/example/fragments/Fragment2.kt
@@ -1,6 +1,8 @@
 package com.example.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -16,8 +18,75 @@ private const val ARG_PARAM2 = "param2"
  * Use the [Fragment1.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Fragment2 : Fragment(R.layout.fragment_2) {
-   
+class Fragment2 : Fragment() {
+    private val TAG = "Blue Fragment"
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.i(TAG, "onAttach")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i(TAG, "onCreate")
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val v = inflater.inflate(R.layout.fragment_2, container, false)
+        Log.i(TAG, "onCreateView")
+        return v
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.i(TAG, "onDestroyView")
+    }
+
+    override fun onPause() {
+        //DO NOTHING
+        super.onPause()
+        Log.i(TAG, "onPause")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.i(TAG, "onSaveInstanceState")
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        Log.i(TAG, "onViewStateRestored")
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        Log.i(TAG, "onActivityCreated")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.i(TAG, "onDetach")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy")
+    }
 
 
 
